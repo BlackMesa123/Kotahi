84509d329230b1c5a79091ae03a8db0b231f8429
BlackMesa123 <giangrecosalvo9@gmail.com>
2022-06-18 20:52:40 +0200

    utils: stub: init
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

---
+++ utils/stub/src/main/java/android/os/SemBuild.java
@@ -0,0 +1,15 @@
+package android.os;
+
+import dev.rikka.tools.refine.RefineAs;
+
+@RefineAs(Build.class)
+public class SemBuild {
+
+    public static class VERSION {
+
+        public static /*final*/ int SEM_INT;
+        public static /*final*/ int SEM_PLATFORM_INT;
+
+    }
+
+}
